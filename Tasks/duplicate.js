f1090ce6a9e5190f60d95fbc1b26e8d8390ed8cf
Oleksandr <78824330+sashko2004-57@users.noreply.github.com>
2024-01-15 12:24:15 +0200

    Remove unneeded code
    Also, I have fixed my mistake(I forgot to change one occurence of "i" to "counter")

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -3,14 +3,11 @@
 'use strict';
 
 const duplicateValue = (value, numberOfDuplicates) => {
-  if (numberOfDuplicates <= 0) return [];
-  else {
-    let arrayWithDuplicates = [];
-    for (let counter = 0; counter < numberOfDuplicates; counter++) {
-      arrayWithDuplicates[i] = value;
-    }
-    return arrayWithDuplicates;
+  let arrayWithDuplicates = [];
+  for (let counter = 0; counter < numberOfDuplicates; counter++) {
+    arrayWithDuplicates[counter] = value;
   }
+  return arrayWithDuplicates;
 };
 
 module.exports = duplicateValue;

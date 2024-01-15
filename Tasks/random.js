e23a9660dbed390e375e63067d01d1feae8d44ab
Oleksandr <78824330+sashko2004-57@users.noreply.github.com>
2024-01-15 11:51:15 +0200

    Remove checking of "max === undefined" condition
    I think that this checking is odd

--- Tasks/random.js
+++ Tasks/random.js
@@ -3,12 +3,7 @@
 'use strict';
 
 const random = (min, max) => {
-  if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
-    return min + Math.floor(Math.random() * (max - min + 1));
-  }
+  return min + Math.floor(Math.random() * (max - min + 1));
 };
 
 module.exports = random;

d1dd7bc3c91433dbbcf73c2fe9c40e79ac29de32
Oleksandr <78824330+sashko2004-57@users.noreply.github.com>
2024-01-15 16:13:19 +0200

    Use ternary operator

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -8,9 +8,7 @@ const sumNumbersFromArray = function (array) {
   let sumValue = 0;
   for (const element of array) {
     let typeOfElement = typeof element;
-    if (typeOfElement === numberTypeName) {
-      sumValue += element;
-    }
+    sumValue += (typeOfElement === numberTypeName) ? element : 0;
   }
   return sumValue;
 };
